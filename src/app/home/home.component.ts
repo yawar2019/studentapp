20fa829b7e7b28850a63921181781153b2965ccf
Yawar2019 <yawarali17@gmail.com>
2024-12-28 21:05:40 +0530

    committed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { AboutComponent } from '../about/about.component';
 import { first } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [AppComponent,AboutComponent],
+  imports: [AppComponent,AboutComponent,FormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 alphabet="z";
+isdirectavail=false;
 carlist=['BMW','Tata','TesLa','Nano'];
 sports=[
   {id:1,name:"Virat"},
@@ -18,13 +21,42 @@ sports=[
   {id:3,name:"Yuvi"}
 ];
 
-username="bhavni";
+username="Ananad";
+TwoWayBindVar="Hello two way bind";
 myfirstclass="first";
-mysecondclass="second";
-isFirstClassAvailable=true;
+mysecondclass="second"; 
+isFirstClassAvailable=false;
 myfirstclassarr=["first","second"];
 myfirstclassObj={
   first:true,
   second:true
 }
+myFirstcolor='Green';
+myAllStyle={
+  "color":"Yellow",
+  "background-color":"blue"
+}
+
+
+fun1()
+{
+   this.username="Ravi";
+}
+
+fun2(name:string)
+{
+  alert('hello world '+name);
+}
+
+fun3(event:any)
+{
+  console.log(event);
+}
+
+Myfun(val:string)
+{
+
+  alert(val);
+}
+
 }
