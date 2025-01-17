41ce2b0f9e7432d22e161628a0b29835ba7082b6
Yawar2019 <yawarali17@gmail.com>
2025-01-17 22:03:09 +0530

    query params

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,17 +12,49 @@ user={
   id:"",
   name:""
 }
+
+pagfilter={
+  page:"",
+  search:""
+}
+fragmentDetail:string|null="";
   constructor(private route:ActivatedRoute)
 {
-
+  console.log("constructor called");
 }
 
 
   ngOnInit(): void {
-   this.user={
-    id:this.route.snapshot.params['id'],
-    name:this.route.snapshot.params['name']
-   }
+  //  this.user={
+  //   id:this.route.snapshot.params['id'],
+  //   name:this.route.snapshot.params['name']
+  //  }
+  console.log("ngOninit called");
+   console.log(this.route.snapshot.queryParams);
+
+
+   this.route.params.subscribe((data)=>{
+    this.user={
+      id:data['id'],
+      name:data['name']
+     }
+
+   })
+
+
+   this.route.queryParams.subscribe((data)=>{
+    this.pagfilter={
+      page:data['page'],
+      search:data['search']
+    }
+
+   })
+
+   this.route.fragment.subscribe((data)=>{
+this.fragmentDetail=data;    
+ 
+   })
+
 
   }
 
